ba3bc7b9ea94fa656ab721e2a25cf8ec6ff217a0
Alexandre Chouippe <alexandre.chouippe@doctolib.com>
2024-07-18 18:00:46 +0200

    Rework the way to inject configuration to support env variables

---
+++ config/runtime.exs
@@ -0,0 +1,54 @@
+import Config
+
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  level: String.to_atom(System.get_env("LOG_LEVEL") || "info")
+
+config :neurow, public_api_port: String.to_integer(System.get_env("PUBLIC_API_PORT") || "4000")
+
+config :neurow,
+  internal_api_port: String.to_integer(System.get_env("INTERNAL_API_PORT") || "3000")
+
+config :neurow, sse_timeout: String.to_integer(System.get_env("SSE_TIMEOUT") || "900000")
+
+config :neurow, ssl_keyfile: System.get_env("SSL_KEYFILE")
+config :neurow, ssl_certfile: System.get_env("SSL_CERTFILE")
+
+case config_env() do
+  :prod ->
+    {:ok, interservice_json_config} = Jason.decode(System.fetch_env!("INTERSERVICE_SERVER_NEURO_CONFIG"))
+
+    config :neurow, public_api_authentication: %{
+      verbose_authentication_errors: false,
+      audience: interservice_json_config["service_name"],
+      issuers: interservice_json_config["clients"]
+    }
+
+    config :neurow, internal_api_authentication: %{
+      verbose_authentication_errors: false,
+      audience: interservice_json_config["service_name"],
+      issuers: interservice_json_config["clients"]
+    }
+
+  env when env in [:dev, :test] ->
+    config :neurow,
+      public_api_authentication: %{
+        verbose_authentication_errors: true,
+        audience: "public_api",
+        issuers: %{
+          test_issuer1: "966KljJz--KyzyBnMOrFXfAkq9XMqWwPgdBV3cKTxsc",
+          test_issuer2: "fu5E9VxCL8nhMG7jT4IXv3xarX8WIT7R-1pWFGm-sVw"
+        }
+      }
+
+    config :neurow,
+      internal_api_authentication: %{
+        verbose_authentication_errors: true,
+        audience: "internal_api",
+        issuers: %{
+          test_issuer1: "nLjJdNLlpdv3W4Xk7MyVCAZKD-hvza6FQ4yhUUFnjmg",
+          test_issuer2: "3opQEJI3WK9ovGm9pHUQ6I3SkjlDYWZUeAUSazjv05g"
+        }
+      }
+  _ -> raise "Unsupported environment"
+end
