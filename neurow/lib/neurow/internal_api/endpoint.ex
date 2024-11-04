e39f3a67a280579b2ce80e6d849218f8656219c8
Alexandre Chouippe <alexandre.chouippe@doctolib.com>
2024-11-04 14:57:39 +0100

    Count memory usage per category of process and make it available on the internal API

--- neurow/lib/neurow/internal_api/endpoint.ex
+++ neurow/lib/neurow/internal_api/endpoint.ex
@@ -17,7 +17,14 @@ defmodule Neurow.InternalApi.Endpoint do
     max_lifetime: &Neurow.Configuration.internal_api_jwt_max_lifetime/0,
     send_forbidden: &Neurow.InternalApi.Endpoint.send_forbidden/3,
     inc_error_callback: &Neurow.Stats.Security.inc_jwt_errors_internal/0,
-    exclude_path_prefixes: ["/ping", "/nodes", "/cluster_size_above", "/history"]
+    exclude_path_prefixes: [
+      "/ping",
+      "/nodes",
+      "/cluster_size_above",
+      "/history",
+      "/process_stats",
+      "/favicon.ico"
+    ]
   )
 
   plug(:match)
@@ -52,6 +59,15 @@ defmodule Neurow.InternalApi.Endpoint do
     |> send_resp(
       200,
       "Current node: #{node()}\r\nTotal nodes: #{length(nodes) + 1}\r\nNodes: #{inspect(nodes)}\r\n"
+    )
+  end
+
+  get "/process_stats" do
+    conn
+    |> put_resp_header("content-type", "application/json")
+    |> send_resp(
+      200,
+      Jason.encode!(Neurow.Stats.Processes.grouping_stats() |> Enum.map(&Map.from_struct/1))
     )
   end
 
--- neurow/lib/stats.ex
+++ neurow/lib/stats.ex
@@ -175,4 +175,84 @@ defmodule Neurow.Stats do
       Boolean.set([name: :stopping], true)
     end
   end
+
+  defmodule Processes do
+    defmodule ProcessesStats do
+      defstruct [
+        :name_or_initial_func,
+        :current_func,
+        process_count: 0,
+        memory: 0,
+        message_queue_len: 0
+      ]
+    end
+
+    def grouping_stats(result_count \\ 40) do
+      Process.list()
+      |> Enum.reduce(%{}, fn pid, acc ->
+        {name_or_initial_func, current_func} = grouping_attributes(pid)
+
+        process_info =
+          Process.info(pid, [
+            :memory,
+            :message_queue_len
+          ])
+
+        current_stats =
+          acc[{name_or_initial_func, current_func}] ||
+            %ProcessesStats{
+              name_or_initial_func: name_or_initial_func,
+              current_func: current_func
+            }
+
+        process_count = current_stats.process_count + 1
+        memory = current_stats.memory + (process_info[:memory] || 0)
+
+        message_queue_len =
+          current_stats.message_queue_len + (process_info[:message_queue_len] || 0)
+
+        Map.put(acc, {name_or_initial_func, current_func}, %ProcessesStats{
+          name_or_initial_func: name_or_initial_func,
+          process_count: process_count,
+          current_func: current_func,
+          memory: memory,
+          message_queue_len: message_queue_len
+        })
+      end)
+      |> Map.values()
+      |> Enum.sort(&(&1.memory > &2.memory))
+      |> Enum.take(result_count)
+    end
+
+    defp mfa_to_string({module, function, arity}) do
+      "#{module}:#{function}/#{arity}"
+    end
+
+    defp grouping_attributes(pid) do
+      name_or_initial_func =
+        case Process.info(pid, [:registered_name, :dictionary, :initial_call]) do
+          [{:registered_name, name} | _rest] when is_atom(name) ->
+            name
+
+          [{:registered_name, [first_name | _other_names]}, _rest] ->
+            first_name
+
+          [
+            {:registered_name, []},
+            {:dictionary, [{:"$initial_call", initial_call} | _rest_dictionary]} | _rest
+          ] ->
+            mfa_to_string(initial_call)
+
+          [
+            {:registered_name, []},
+            {:dictionary, _rest_dictionary},
+            {:initial_call, initial_call}
+          ] ->
+            mfa_to_string(initial_call)
+        end
+
+      {:current_function, current_function} = Process.info(pid, :current_function)
+      {name_or_initial_func, mfa_to_string(current_function)}
+    end
+  end
 end
