11afe90dc862099f1d14df8cead865c5d6e04817
Wilfried OLLIVIER <wilfried.ollivier@doctolib.com>
2025-01-31 08:58:05 +0100

    chore(bump-elixir): Bump Elixir to 1.18 (#50)

--- neurow/mix.exs
+++ neurow/mix.exs
@@ -5,7 +5,7 @@ defmodule Neurow.MixProject do
     [
       app: :neurow,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_paths: [
